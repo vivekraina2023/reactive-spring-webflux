94c9a28214a848df39426b41604b4e32df22df60
vivekraina2023 <vivekraina2023@gmail.com>
2023-09-06 10:28:23 -0400

    added docker file to support docker image deployment v2.0

--- movies-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
+++ movies-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoviesInfoServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesInfoServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(MoviesInfoServiceApplication.class, args);
+
+
+    }
 
 }
